4a8db64179efbf43113984f973cd0e2fb0d300d7
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-05-13 16:12:57 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+AndroidSeminar
+==============
+
+Android セミナー
---
+++ src/com/sample/androidseminar/AndroidSeminarActivity.java
@@ -0,0 +1,13 @@
+package com.sample.androidseminar;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidSeminarActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
